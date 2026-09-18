d7f555ad80d5b3cf4239080536c0ea3868fbf268
fabrigod939 <Fabrigod939@gmail.com>
2026-09-17 21:20:14 -0300

    add go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fabrigod939/Calculadora.go
+
+go 1.22.2
---
+++ operação/operação.go
@@ -0,0 +1,31 @@
+package operação
+
+import "fmt"
+
+type Soma struct{}
+
+func (s Soma) Calcular(a, b float64) float64 {
+	return a + b
+}
+
+type Subtração struct{}
+
+func (s Subtração) Calcular(a, b float64) float64 {
+	return a - b
+}
+
+type Multiplicação struct{}
+
+func (m Multiplicação) Calcular(a, b float64) float64 {
+	return a * b
+}
+
+type Divisão struct{}
+
+func (d Divisão) Calcular(a, b float64) float64 {
+	if b == 0 {
+		fmt.Println("Operação Invalida")
+		return 0
+	}
+	return a / b
+}
